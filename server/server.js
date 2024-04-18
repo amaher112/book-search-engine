b79c50d7bd5f561c243833fb55d0de3ff8c1b237
amaher <amaher112@su.edu>
2024-04-17 20:11:10 -0400

    'refactor original code to accept graphql'

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -21,6 +22,10 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
@@ -30,8 +35,7 @@ const startApolloServer = async () => {
     });
   }
 
-  // Delete this later:
-  app.use(routes);
+ 
 
   db.once("open", () => {
     app.listen(PORT, () => {
